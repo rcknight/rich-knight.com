e0a9b42f9bb3850734b8d5f45cf2fb594a115b45
Rich Knight <rich@rich-knight.com>
2015-04-02 13:19:25 +0100

    added update script

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Specify tag as first param"
+    exit
+fi
+
+git checkout upstream-master
+
+rm -rf ./*
+
+curl -L -o $1.tar.gz https://github.com/johnotander/pixyll/archive/$1.tar.gz
+tar -xvf $1.tar.gz
+mv pixyll-$1/* .
+mv pixyll-$1/.* .
+rm $1.tar.gz
+rm -fr pixyll-$1
+
+git add .
+git commit -m "Update pixyll to $1"
+
+git checkout master
+git rebase upstream-masterls
